d74eaf2f30d575e4f9b46c1267547b26271a1be7
Matt Burton <mattjburton64@gmail.com>
2024-01-27 16:01:06 -0500

    turn around time function

---
+++ turn-around-time.py
@@ -0,0 +1,13 @@
+def TurnAroundTime(refueling):
+    # time breakdown
+    timeToDisembarkPassengers = 15
+    timeToCleanAircraftAndChangeCrew = 10
+    timeToBoardPassengers = 15
+    timeToRefuel = 10
+
+    # time colculation
+    totalTurnAroundTime = timeToDisembarkPassengers + timeToCleanAircraftAndChangeCrew + timeToBoardPassengers
+    if refueling:
+        totalTurnAroundTime += timeToRefuel
+
+    return totalTurnAroundTime
