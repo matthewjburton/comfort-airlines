e0b0d865e2724405e2ccf3d5cd8557153a3fb20c
Matt Burton <mattjburton64@gmail.com>
2024-02-01 09:41:07 -0500

    file header

--- turn-around-time.py
+++ turn-around-time.py
@@ -1,3 +1,9 @@
+# Purpose:  Return the minimum amount of time in minutes that an
+#           aircraft must wait before taking off for its next flight
+# Author:   Matt Burton
+# Notes:    This function will be in the simulation immediately after a flight lands
+#           Consider moving this function to an event scheduler or the aircraft class
+
 def TurnAroundTime(refueling):
     # time breakdown
     timeToDisembarkPassengers = 15
