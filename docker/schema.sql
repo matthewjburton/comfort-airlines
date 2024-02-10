bcb5f70c86c0bca1851a120083bd1af217bca639
jchenHPU <jchen@highpoint.edu>
2024-02-10 17:13:26 -0500

    aircraft info and update schema to match compacity for seats.

--- docker/schema.sql
+++ docker/schema.sql
@@ -13,17 +13,19 @@ CREATE TABLE airports (
     longitude FLOAT,
     timezone_offset INT,
     metro_population INT,
-    city VARCHAR(255),
-    state VARCHAR(255)
+    total_gates INT,
+    is_hub BINARY
 );
 
 CREATE TABLE aircraft (
     aircraft_id INT AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(255),
     tail_number VARCHAR(20),
+    name VARCHAR(255),
+    model VARCHAR(255),
     maximum_speed INT,
-    maximum_capacity INT,
+    maximum_capacity VARCHAR(20),
     maximum_fuel INT,
+    cargo_volume INT,
     leasing_cost INT
 );
 
@@ -31,23 +33,28 @@ CREATE TABLE flights (
     flight_id INT AUTO_INCREMENT PRIMARY KEY,
     departure_airport_id INT,
     destination_airport_id INT,
-    direction_of_flight FLOAT,
+    angle_of_flight FLOAT,
     flight_duration_minutes INT,
-    departure_time INT,
-    arrival_time INT,
+    local_departure_time INT,
+    local_arrival_time INT,
+    aircraft_id VARCHAR(20),
+    flight_number INT,
+    available_seats INT,
+    on_time_bin BINARY,
+    gate_departure INT,
+    gate_arrival INT,
+    FOREIGN KEY (aircraft_id) REFERENCES aircraft(aircraft_id),
     FOREIGN KEY (departure_airport_id) REFERENCES airports(airport_id),
     FOREIGN KEY (destination_airport_id) REFERENCES airports(airport_id)
 );
 
 CREATE TABLE routes (
     route_id INT AUTO_INCREMENT PRIMARY KEY,
-    layover_times JSON
-);
-
-CREATE TABLE flights_routes (
-    flight_id INT,
-    route_id INT,
-    FOREIGN KEY (flight_id) REFERENCES flights(flight_id),
-    FOREIGN KEY (route_id) REFERENCES routes(route_id),
-    PRIMARY KEY (flight_id, route_id)
+    layover_time INT[],
+    starting_airport_id INT,
+    destination_airport_id INT,
+    flight_legs_ids INT[],
+    FOREIGN KEY (starting_airport_id) REFERENCES airports(airport_id),
+    FOREIGN KEY (destination_airport_id) REFERENCES airports(airport_id),
+    FOREIGN KEY (flight_legs_ids) REFERENCES flights(flight_id)
 );
