709159b6fa6dd3ad7366b72413210a5c8d95f6d7
jmaas1 <jmaas@highpoint.edu>
2024-02-16 15:13:39 -0500

    Testing is verified, Logic is done, Only step is to create Airport class and pull from that rather than built in temp class

--- flight-angle.py
+++ flight-angle.py
@@ -1,30 +1,73 @@
-#This file will be used to calculate a saved % of time of a flight based on the angle of the flight and the winds
-#will calculate gain or loss based on wind direction + flight angle
-#THIS IS NON FUNCTIONAL ATM (need to add in way to grab lat and longitude values)
+#This file will be used to calculate the % of flight time that the flight will actually take
+#It will calculate gain or loss based on wind direction + bearing angle
+
 
 import math
 
+class Airport:#Temp Airport class
+    def __init__(self, latitude, longitude):
+        self.latitude = latitude
+        self.longitude = longitude
+
 # Returns a float (negative for time saved, positive for time gained)
-def calculatePercentage(start, end, wind=.045):
+def calculatePercentage(startAirport, endAirport, wind=.045):
 
-    direction = 1.0
-    offset = abs((start.lat - end.lat)*69.4) #Gives latitude distance in miles
-    horizontalDistance = abs((start.long-end.long)*69.4) #Gives longitiude distance in miles
+    dif = endAirport.longitude-startAirport.longitude
 
-    tangentPart = offset/horizontalDistance
+    #Edge case: Flight is perfectly vertical, wind has no effect
+    if(dif == 0):
+        return 1
 
-    angle = math.atan(tangentPart)
-    angle = angle*(180/math.pi)
+    #Edge case: Flight is perfectly horizontal, wind has full effect
+    if(startAirport.latitude == endAirport.latitude):
+        if(dif<0):
+            return 1+wind #East to West, Time added
+        else:
+            return 1-wind #West to East, Time saved
 
-    if angle > 90:
-        angle = angle - 90
+    #Math to determine the bearing angle: will yield value between -180 to 180
+    X = math.cos(math.radians(endAirport.latitude)) * math.sin(math.radians(dif))
+    Y = math.cos(math.radians(startAirport.latitude)) * math.sin(math.radians(endAirport.latitude)) - math.sin(math.radians(startAirport.latitude) )* math.cos(math.radians(endAirport.latitude)) * math.cos(math.radians(dif))
 
-    result = angle/90
+    Z = math.degrees(math.atan2(X,Y))#Z is the variable that will be manipulated, as it is the bearing angle between airports
 
-    if start.long > end.long:
-        return result*wind#Time lost
 
-    elif end.long > start.long:
-        return result*wind*-1#Time gained
-    else:
-        return 0
+    if(Z<0): #Flight direction is : East to West
+        Z = Z/90 #Gets percentage of 90(Full wind speed) that the flight angle contains
+        if(Z< -1): #If it is below negative one, need to do additional calculations to get correct %
+            Z = -1*(Z + 2)
+
+        Z = 1+(Z*-1*wind)#Since negative number indicates % increase in flight time, change % to positive, mulitply by wind, and add one to find % of base flight time this flight will have
+
+    else:#Flight direction is: West to East
+        Z = Z/90#Gets percentage of 90(Full wind speed) that the flight angle contains
+        if (Z > 1): #If it is above one, need to do additional calculations to get correct %
+            Z = (1 - Z) + 1
+
+        Z = 1-(Z*wind)#Since % number indicates % decrease in flight time, multiply % by wind, and subtract from 1 to get % time of base flight time this flight will have
+
+
+    return Z #Multiply this result by flight time to get actual flight time accounting for wind
+
+#Test Airports below
+
+JFK = Airport(40.641766, -73.780968)
+LAX = Airport(33.9416, -118.4085)
+ORD = Airport(41.978611, -87.904724)
+DFW = Airport(32.89748, -97.040443)
+KC = Airport(39.099912,-94.581213)
+SL = Airport(38.627089,-90.200203)
+CHI = Airport(41.8781,-87.6298)
+MEM = Airport(35.1495,-90.0490)
+
+print(f"% of base flight time based on wind for JFK and LAX:")
+print(calculatePercentage(JFK, LAX))
+print(f"% of base flight time based on wind for JFK and ORD:")
+print(calculatePercentage(JFK, ORD))
+print(f"% of base flight time based on wind for JFK and DFW:")
+print(calculatePercentage(JFK, DFW))
+print(f"% of base flight time based on wind for KC and SL:")
+print(calculatePercentage(KC, SL))
+print(f"% of base flight time based on wind for CHI and MEM:")
+print(calculatePercentage(CHI, MEM))
+
