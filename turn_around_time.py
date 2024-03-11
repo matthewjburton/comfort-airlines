6f294c84780a8ef05ccdfc0804b768034764ffe9
Matt Burton <mattjburton64@gmail.com>
2024-03-11 13:44:20 -0400

    turn around time constants moved to top

--- turn_around_time.py
+++ turn_around_time.py
@@ -8,20 +8,20 @@ __author__ = Matt Burton
 Notes:
     This function will be in the simulation immediately after a flight lands
     Consider moving this function to an event scheduler or the aircraft class
+    Consider changing the parameter passed in to simply the aircraft and access its fuel level to determine if it needs to be refueled
 """
+# Time in minutes
+DISEMBARK = 15          # time for passengers to disembark
+CLEAN_AND_CREW = 10     # time to clean the aircraft and change the crew
+BOARDING = 15           # time for passsengers to board the aircraft
+REFUEL = 10             # time to refuel the aircraft
 
-def turn_around_time(aircraftNeedsRefueling):
-    # Time breakdown
-    timeToDisembarkPassengers = 15
-    timeToCleanAircraftAndChangeCrew = 10
-    timeToBoardPassengers = 15
-    timeToRefuel = 10
-
+def turn_around_time(aircraftNeedsRefuel):
     # Time calculation
-    totalTurnAroundTime = timeToDisembarkPassengers + timeToCleanAircraftAndChangeCrew + timeToBoardPassengers
+    turnAroundTime = DISEMBARK + CLEAN_AND_CREW + BOARDING
 
     # Account for refueling
-    if aircraftNeedsRefueling:
-        totalTurnAroundTime += timeToRefuel
+    if aircraftNeedsRefuel:
+        turnAroundTime += REFUEL
 
-    return totalTurnAroundTime
+    return turnAroundTime
