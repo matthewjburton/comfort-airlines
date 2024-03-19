56cd3f6b4d8dbf658c78d511b2a890731d68998b
macro10 <mchaletrotter22@gmail.com>
2024-03-19 19:56:11 -0400

    Added skin and bones of aircraft.py menu

---
+++ aircraft.py
@@ -0,0 +1,28 @@
+"""
+Class responsible for implementing the menu options under the aircraft menu option
+
+__team_name__ = Cloud Nine
+__team_members__ = Jeremy Maas, Matt Burton, McHale Trotter, Kevin Sampson, Justin Chen, Ryan Hirscher
+__author__ = McHale Trotter
+"""
+
+class AircraftMenu:
+
+    """Aircraft Options"""
+    @staticmethod
+    def view_aircraft():
+        print("\nExecuting view_aircraft()")
+
+    @staticmethod
+    def edit_aircraft():
+        print("\nExecuting edit_aircraft()")
+        
+    
+    @staticmethod
+    def add_aircraft():
+        print("\nExecuting add_aircraft()")
+
+    @staticmethod
+    def remove_aircraft():
+        print("\nExecuting remove_aircraft()")
+    
