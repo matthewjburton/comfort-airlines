84c1a21a9bad428441c9d28721ba692c7141f705
jchenHPU <jchen@highpoint.edu>
2024-03-25 13:04:35 -0400

    modify flight takeoff with conversion of knots to mph and also comments

--- flight_takeoff.py
+++ flight_takeoff.py
@@ -1,7 +1,17 @@
+"""
+2 functions first one calculates the time it takes to fly and get into cruising altitude then to reach max speed
+
+the second function gets the amount of time it takes to descend down.
+
+__team_name__ = Cloud Nine
+__team_members__ = Jeremy Maas, Matt Burton, McHale Trotter, Kevin Sampson, Justin Chen, Ryan Hirscher
+__author__ = Justin Chen
+"""
+
 import math
 import airport
 
-
+"""
 def calculate_taxi_time(population, is_hub=False):
     if is_hub:
         if population <= 9000000:
@@ -12,6 +22,7 @@ def calculate_taxi_time(population, is_hub=False):
     else:
         taxi_time = min(13, population * 0.00075)
         return min(taxi_time, 13)
+"""
 
 def calculate_takeoff_time():
     return 1
@@ -20,12 +31,12 @@ def calculate_landing_time():
     return 2
 
 def calculate_acceleration_time(cruising_altitude, max_speed):
-    time_to_cruise_altitude = (cruising_altitude - 10000) / 280  # time to ascend to cruising altitude
-    time_to_max_speed = (max_speed * 0.8 - 280) / 25  # time to accelerate to 80% of max speed
+    time_to_cruise_altitude = (cruising_altitude - 10000) / (250 * 1.15)  # time to ascend to cruising altitude in minutes
+    time_to_max_speed = (max_speed * 0.8 / 1.15 - 280) / (25 * 1.15)  # time to accelerate to 80% of max speed in minutes
     return time_to_cruise_altitude + time_to_max_speed
 
 def calculate_descent_time(distance):
-    descent_rate = 1000 / 3  # descent rate in feet per nautical mile
+    descent_rate = 1000 / (3 * 1.15)  # descent rate in feet per nautical mile per minute
     if distance >= 1500:
         cruising_altitude = 35000
     elif distance >= 350:
@@ -34,10 +45,11 @@ def calculate_descent_time(distance):
         cruising_altitude = 25000
     else:
         cruising_altitude = 20000
-    time_to_cruise_altitude = (cruising_altitude - 10000) / 250  # time to descend to cruising altitude
-    time_to_land = (distance * 1.852) / 200  # time to descend at 200 knots
+    time_to_cruise_altitude = (cruising_altitude - 10000) / (250 * 1.15)  # time to descend to cruising altitude in minutes
+    time_to_land = (distance * 1.151) / (200 * 1.15)  # time to descend at 200 miles per hour in minutes
     return time_to_cruise_altitude + time_to_land
 
+"""
 # Example usage:
 population = 5000000  # Example metro area population
 is_hub = False  # Example airport type
@@ -52,3 +64,4 @@ descent_time = calculate_descent_time(distance)
 
 total_flight_time = taxi_time + takeoff_time + landing_time + acceleration_time + descent_time
 print(f"Total flight time: {total_flight_time} minutes")
+"""
