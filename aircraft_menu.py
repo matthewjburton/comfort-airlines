2a7d71eaad6138f57faf6938b7c585232e5fb648
macro10 <mchaletrotter22@gmail.com>
2024-03-26 18:06:46 -0400

    added 'add_aircraft()', 'remove_aircraft()', and 'view_aircraft()'

--- aircraft_menu.py
+++ aircraft_menu.py
@@ -6,12 +6,30 @@ __team_members__ = Jeremy Maas, Matt Burton, McHale Trotter, Kevin Sampson, Just
 __author__ = McHale Trotter and Matt Burton
 """
 from menu import display_menu
+import database
 class AircraftMenu:
 
     """Aircraft Options"""
     @staticmethod
     def view_aircraft():
         print("\nExecuting view_aircraft()")
+        # Initialize the Database object
+        db = database.Database()
+
+        # Show all aircraft in the database for reference
+        sql = "SELECT * FROM aircraft"
+        
+        try:
+            # Execute the select query, (returns dataframe)
+            df = db.execute_query_to_dataframe(sql)
+            # Print the dataframe
+            print(df)
+        except Exception as e:
+            print(f"Error printing aircraft table: {e}")
+        finally:
+            # Disconnect from the database
+            db.disconnect()
+
 
     @staticmethod
     def edit_aircraft():
@@ -23,12 +41,67 @@ class AircraftMenu:
         display_menu(edit_options, is_submenu = True)
     
     
-    """Edit Options"""
+    """
+    Edit Options
+    add_aircraft: adds an aircraft from user input. (No input sanitization yet)
+    remove_aircraft: prints the aircraft table and then allows user to remove an aircraft by aircraft_id.
+    """
     @staticmethod
     def add_aircraft():
         print("\nExecuting add_aircraft()")
+        # Initialize the Database object
+        db = database.Database()
+
+        # Get input from the user
+        tail_number = input("Enter tail number: ")
+        name = input("Enter name: ")
+        model = input("Enter model: ")
+        maximum_speed = int(input("Enter maximum speed (miles per hour): "))
+        maximum_capacity = int(input("Enter maximum capacity (number of people): "))
+        maximum_fuel = int(input("Enter maximum fuel (amount in gallons): "))
+        cargo_volume = int(input("Enter cargo volume (volume in cubic feet): "))
+        leasing_cost = int(input("Enter leasing cost (price in USD): "))
+
+        # SQL query to insert data into the aircraft table
+        sql = f"INSERT INTO aircraft (tail_number, name, model, maximum_speed, maximum_capacity, maximum_fuel, cargo_volume, leasing_cost) VALUES ('{tail_number}', '{name}', '{model}', {maximum_speed}, {maximum_capacity}, {maximum_fuel}, {cargo_volume}, {leasing_cost})"
+
+        try:
+            # Execute the insert query
+            db.execute_insert_update_delete_query(sql)
+            print("Aircraft added successfully!")
+        except Exception as e:
+            print(f"Error adding aircraft: {e}")
+        finally:
+            # Disconnect from the database
+            db.disconnect()
 
     @staticmethod
     def remove_aircraft():
         print("\nExecuting remove_aircraft()")
-    
+        # Print the aircraft table
+        AircraftMenu.view_aircraft()
+
+        # Initialize the Database object
+        db = database.Database()
+
+        # User input for aircraft to be remove based on aircraft_id
+        aircraft_id = input("Enter aircraft ID number to remove: ")
+
+        sql = f"DELETE FROM aircraft WHERE aircraft_id = {aircraft_id}"
+
+        try: 
+            # Execute remove aircraft query
+            db.execute_insert_update_delete_query(sql)
+            print("Aircraft removed successfully!")
+        except Exception as e:
+            print(f"Error removing aircraft: {e}")
+        finally:
+            # Disconnect from the database
+            db.disconnect()
+
+
+# Function calls for testing
+
+# AircraftMenu.add_aircraft()
+# AircraftMenu.remove_aircraft()
+# AircraftMenu.view_aircraft()
