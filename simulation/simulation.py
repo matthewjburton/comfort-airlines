2113811eebd2b4496bef584afa3c083c7c352b15
Matt Burton <mattjburton64@gmail.com>
2024-03-27 16:49:06 -0400

    simulation working with  reduced clock and object dictionaries

--- simulation/simulation.py
+++ simulation/simulation.py
@@ -5,11 +5,9 @@ __team_name__ = Cloud Nine
 __team_members__ = Jeremy Maas, Matt Burton, McHale Trotter, Kevin Sampson, Justin Chen, Ryan Hirscher
 __author__ = Matt Burton
 """
-from utilities.clock import print_time
 from utilities.database import Database
+from utilities.clock import print_time
 
-from objects.airport import Airport
-from objects.aircraft import Aircraft
 from objects.flight import Flight
 
 from .schedule import Schedule
@@ -28,7 +26,7 @@ class Simulation:
         for minute in range(MINUTES_IN_A_DAY):
             currentEvents = schedule.get_events_for_minute(minute)
             for currentEvent in currentEvents:
-                print(f"{print_time(minute)}: {currentEvent.execute()}")       
+                print(f"{print_time(minute)}: {currentEvent.execute()}")    
 
     @staticmethod
     def populate_schedule_from_timetable(schedule):
@@ -36,8 +34,10 @@ class Simulation:
 
         # Populate the schedule queue with events based on the timetable
         for flight in timetable:
-            schedule.add_event(DepartureEvent(flight))
-            schedule.add_event(ArrivalEvent(flight))
+            schedule.add_event(DepartureEvent(timetable[flight]))
+            schedule.add_event(ArrivalEvent(timetable[flight]))
+
+        return schedule
 
     @staticmethod
     def create_timetable_from_database():
@@ -47,9 +47,9 @@ class Simulation:
         dataframe = db.execute_query_to_dataframe(query)
 
         # Create Flight objects and store them in a list
-        timetable = []
+        timetable = {}
         for _, row in dataframe.iterrows():
-            flight = Flight(id=row['flight_id'], number=row['flight_number'], aircraftID=row['aircraft_id'], departureAirportID=row['departure_airport_id'], destinationAirportID=row['destination_airport_id'], angleOfFlight=row['angle_of_flight'], duration=row['flight_duration_minutes'], localDepartureTime=row['local_departure_time'], localArrivalTime=row['local_arrival_time'], onTimeBin=row['on_time_bin'], gateDeparture=row['gate_departure'], gateArrival=row['gate_arrival'])
-            timetable.append(flight)
+            flight = Flight(row['flight_id'], row['flight_number'], row['aircraft_id'], row['departure_airport_id'], row['destination_airport_id'], row['angle_of_flight'], row['flight_duration_minutes'], row['local_departure_time'], row['local_arrival_time'], row['on_time_bin'], row['gate_departure'], row['gate_arrival'])
+            timetable[row['flight_id']] = flight
 
         return timetable
--- utilities/clock.py
+++ utilities/clock.py
@@ -9,12 +9,12 @@ Notes: Currently, no support for daylight savings time
    This class is not meant to be a stand-alone class, instead
    it will be used by the main simulation
 """
-def get_time(minutesIntoTheDay):
-    hours = minutesIntoTheDay // 60
-    minutes = minutesIntoTheDay % 60
-    days = minutesIntoTheDay // (24 * 60)
+def get_time(minutes):
+    hours = minutes // 60
+    minutes = minutes % 60
+    days = minutes // (24 * 60)
     return days, hours, minutes
 
-def print_time(minutesIntoTheDay):
-    days, hours, minutes = get_time(minutesIntoTheDay)
+def print_time(minutes):
+    days, hours, minutes = get_time(minutes)
     return f"Day: {days}, Time: {hours:02}:{minutes:02}"
