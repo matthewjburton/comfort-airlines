d00c5c1dd00a588a5184551e5b75f46748e862b5
Matt Burton <mattjburton64@gmail.com>
2024-03-27 17:34:17 -0400

    reschedule events to later times if there is a conflict

--- simulation/schedule.py
+++ simulation/schedule.py
@@ -5,6 +5,9 @@ __team_name__ = Cloud Nine
 __team_members__ = Jeremy Maas, Matt Burton, McHale Trotter, Kevin Sampson, Justin Chen, Ryan Hirscher
 __author__ = Matt Burton
 """
+
+POSTPONE_TIME = 3
+
 class Schedule:
     def __init__(self):
         self._schedule = {}
@@ -16,6 +19,9 @@ class Schedule:
         minute = event.time  # Assuming event.time represents minutes
         if minute not in self._schedule:
             self._schedule[minute] = []  # Create a list for events at this minute if it doesn't exist
+        
+        self.reschedule_conflicts(minute, event) # Determine if the new events is scheduled to occur at the same time and airport as another sscheduled event
+
         self._schedule[minute].append(event)  # Add the event to the list for this minute
 
     def get_events_for_minute(self, minute):
@@ -23,3 +29,13 @@ class Schedule:
         Retrieve events scheduled for a given minute.
         """
         return self._schedule.get(minute, [])  # Return the list of events for the given minute, or an empty list if no events
+    
+    def reschedule_conflicts(self, minute, event):
+        eventConflict = False
+        for scheduledEvent in self._schedule[minute]:
+            if scheduledEvent.airport == event.airport:
+                eventConflict = True
+
+        if eventConflict:
+            event.time += POSTPONE_TIME
+            self.add_event(event)
