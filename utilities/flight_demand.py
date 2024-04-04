178fef0f18e91d72e7db9405e27f6cb8028b1d67
jmaas1 <jmaas@highpoint.edu>
2024-04-04 14:04:39 -0400

    updated flight demand arithmetic

--- utilities/flight_demand.py
+++ utilities/flight_demand.py
@@ -5,14 +5,22 @@ __team_name__ = Cloud Nine
 __team_members__ = Jeremy Maas, Matt Burton, McHale Trotter, Kevin Sampson, Justin Chen, Ryan Hirscher
 __author__ = Jeremy Maas
 """
-import simulation
+
+from simulation.airport_objects import airports
+from utilities.great_circle import great_circle
 
 PERCENT_FLYING_OUT = 0.005
 
 def individual_demand(startingAirport, endingAirport):
 
+    reachablePopulation = 0
+
+    for airport in airports:
+        if (great_circle(startingAirport, airport) >= 150):
+            reachablePopulation += airport.metro_population()
+
     airportPopulation = endingAirport.metro_population
-    flyerPercent = airportPopulation / simulation.totalPopulation
+    flyerPercent = airportPopulation / reachablePopulation
     
     numFlyingOut = startingAirport.metro_population * PERCENT_FLYING_OUT
 
