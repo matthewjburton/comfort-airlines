9a153576c875f37a6f59673479e635b914156834
Matt Burton <mattjburton64@gmail.com>
2024-04-04 14:39:48 -0400

    linked startDate, duration, and report frequency to the simulation

--- simulation/simulation.py
+++ simulation/simulation.py
@@ -16,23 +16,38 @@ from .scheduled_event import DepartureEvent, ArrivalEvent
 from .aircraft_objects import aircrafts
 from .airport_objects import airports
 
+import os
+import json
+
 MINUTES_IN_A_DAY = 1400
-NUMBER_OF_DAYS = 3
+CONFIG_FILE_PATH = 'simulation/simulation_config.json'
 
 class Simulation:
     @staticmethod
     def run_simulation():
+        # load the simulation configuration dictionary
+        config = Simulation.get_simulation_configuration()
+
         # Get instance of schedule singleton
         schedule = Schedule.get_instance()
 
         # Populate schedule with departure and arrival events
         schedule = Simulation.populate_schedule_from_timetable(schedule)
 
-        # Simulation loop: handle every event for each minute
-        for minute in range(MINUTES_IN_A_DAY * NUMBER_OF_DAYS):
+        # Set simulation start and end times form the simulation config
+        startTime = MINUTES_IN_A_DAY * config.startDate
+        endTime = MINUTES_IN_A_DAY * config.duration
+
+        # MAIN SIMULATION LOOP, process each minute
+        for minute in range(startTime, endTime):
+            # Handle all events for this minute
             currentEvents = schedule.get_events_for_minute(minute)
             for currentEvent in currentEvents:
-                print(f"{print_time(minute)}: {currentEvent.execute()}")    
+                print(f"{print_time(minute)}: {currentEvent.execute()}") 
+
+            # Determine if it's time to generate a report, and generate one if so
+            if Simulation.should_generate_report(config, minute):
+                Simulation.generate_report()
 
     @staticmethod
     def populate_schedule_from_timetable(schedule):
@@ -64,3 +79,31 @@ class Simulation:
             timetable[row['flight_id']] = flight
 
         return timetable
+    
+    def get_simulation_configuration():
+        if os.path.exists(CONFIG_FILE_PATH):
+            with open(CONFIG_FILE_PATH, 'r') as f:
+                return json.load(f)
+        else:
+            return {}
+        
+    def should_generate_report(config, minute):
+        reportFrequency = config.reportFrequency
+        endTime = MINUTES_IN_A_DAY * config.startDate + MINUTES_IN_A_DAY * config.duration
+
+        if reportFrequency is 'daily' and minute % MINUTES_IN_A_DAY == 0:
+            return True
+        elif reportFrequency is 'weekly' and (minute / 7) % MINUTES_IN_A_DAY == 0:
+            return True
+        elif reportFrequency is 'monthly' and (minute / 30) % MINUTES_IN_A_DAY == 0:
+            return True
+        elif reportFrequency is 'yearly' and (minute / 365) % MINUTES_IN_A_DAY == 0:
+            return True
+        elif reportFrequency is 'final' and minute == endTime:
+            return True
+        else:
+            return False
+        
+    def generate_report():
+        print('Executing generate_report()')
+
