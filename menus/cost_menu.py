7dd571b53194927db27309886b46f003ef5bf354
Matt Burton <mattjburton64@gmail.com>
2024-04-04 19:10:33 -0400

    better cost config UI

--- menus/cost_menu.py
+++ menus/cost_menu.py
@@ -24,69 +24,103 @@ class CostMenu:
             return {}
 
     @staticmethod
-    def write_config(config_data):
+    def write_config(config):
         with open(CostMenu.CONFIG_FILE_PATH, 'w') as f:
-            json.dump(config_data, f)
+            json.dump(config, f)
 
     @staticmethod
     def configure_fuel_cost():
-        config_data = CostMenu.read_config()
+        config = CostMenu.read_config()
+
+        # Configuration information for fuel cost
+        print(f"\nConfigure Fuel Cost\n===================\nSet the cost of fuel per gallon\nCurrent fuel cost: ${config.get('fuelCost', 'N/A')} per gallon\n")
+
         while True:
             try:
-                fuelCost = input('Enter the fuel cost (dollars per gallon): $')
+                # Prompt and store user input
+                fuelCost = input("Enter the new fuel cost or 'q' to quit: $")
+
+                # Handle user canceling
+                if fuelCost.lower() == 'q':
+                    print('Input canceled.\n')
+                    break
+
+                # Validate input
                 if CostMenu.is_valid_dollar_value(fuelCost):
                     fuelCost = float(fuelCost)  # Convert to float after validation
                     if 0 <= fuelCost:
                         # Update the fuelCost in the config data
-                        config_data['fuelCost'] = fuelCost
-                        CostMenu.write_config(config_data)
+                        config['fuelCost'] = fuelCost
+                        CostMenu.write_config(config)
                         break
                     else:
-                        print('Fuel cost must be a positive value.')
+                        print('Fuel cost must be a positive value.\n')
                 else:
-                    print('Fuel cost must be a dollar value with two decimal places.')
+                    print('Fuel cost must be a dollar value with two decimal places.\n')
             except ValueError:
-                print("Fuel cost must be a number.")
+                print("Fuel cost must be a number.\n")
 
     @staticmethod
     def configure_takeoff_cost():
-        config_data = CostMenu.read_config()
+        config = CostMenu.read_config()
+
+        # Configuration information for takeoff cost
+        print(f"\nConfigure Takeoff Cost\n======================\nSet the cost per each aircraft takeoff\nCurrent takeoff cost: ${config.get('takeoffCost', 'N/A'):,} per takeoff\n")
+
         while True:
             try:
-                takeoffCost = input('Enter the takeoff cost (dollars per): $')
+                takeoffCost = input("Enter the takeoff cost or 'q' to quit: $")
+
+                # Handle user canceling
+                if takeoffCost.lower() == 'q':
+                    print('Input canceled.\n')
+                    break
+                    
+                # Validate input
                 if not CostMenu.is_valid_dollar_value(takeoffCost):
                     takeoffCost = float(takeoffCost)
                     if 0 <= takeoffCost:
                         # Update the takeoffCost in the config data
-                        config_data['takeoffCost'] = takeoffCost
-                        CostMenu.write_config(config_data)
+                        config['takeoffCost'] = takeoffCost
+                        CostMenu.write_config(config)
                         break
                     else:
-                        print('Takeoff cost must be a positive value.')
+                        print('Takeoff cost must be a positive value.\n')
                 else:
-                    print('Takeoff cost must be a dollar value with two decimal places.')
+                    print('Takeoff cost must be a dollar value with two decimal places.\n')
             except ValueError:
-                print("Takeoff cost must be a number.")
+                print("Takeoff cost must be a number.\n")
 
     @staticmethod
     def configure_landing_cost():
-        config_data = CostMenu.read_config()
+        config = CostMenu.read_config()
+
+        # Configuration information for takeoff cost
+        print(f"\nConfigure Landing Cost\n======================\nSet the cost per each aircraft landing\nCurrent landing cost: ${config.get('landingCost', 'N/A'):,} per landing\n")
+
         while True:
             try:
-                landingCost = input('Enter the landing cost (dollars per): $')
+                landingCost = input("Enter the landing cost or 'q' to quit: $")
+
+                # Handle user canceling
+                if landingCost.lower() == 'q':
+                    print('Input canceled.\n')
+                    break
+
+                #Validate input
                 if not CostMenu.is_valid_dollar_value(landingCost):
                     landingCost = float(landingCost)
                     if 0 <= landingCost:
                         # Update the landingCost in the config data
-                        config_data['landingCost'] = landingCost
-                        CostMenu.write_config(config_data)
+                        config['landingCost'] = landingCost
+                        CostMenu.write_config(config)
                         break
                     else:
-                        print('Landing cost must be a positive value.')
+                        print('Landing cost must be a positive value.\n')
                 else:
-                    print('Landing cost must be a dollar value with two decimal places.')
+                    print('Landing cost must be a dollar value with two decimal places.\n')
             except ValueError:
-                print("Landing cost must be a number.")
+                print("Landing cost must be a number.\n")
 
     @staticmethod
     def configure_aircraft_cost():
