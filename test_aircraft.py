192e1d8667df64befcd767c92b0d0d36218a9da4
Matt Burton <mattjburton64@gmail.com>
2024-04-11 15:06:58 -0400

    remove mchales mistake

--- test_aircraft.py
+++
@@ -1,7 +0,0 @@
-from menus.aircraft_menu import AircraftMenu
-from utilities.database import Database
-
-# AircraftMenu.view_aircraft()
-# AircraftMenu.add_aircraft()
-# AircraftMenu.remove_aircraft()
-
